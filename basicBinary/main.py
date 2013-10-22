a64f48c9a7575777bee804a820a5e4f91d876e98
Paul <paulvstheworld@gmail.com>
2013-10-21 23:30:16 -0400

    added argparse and solution size/character check

--- basicBinary/main.py
+++ basicBinary/main.py
@@ -1,5 +1,35 @@
+#!/usr/bin/env python
+
 from binarygeneticalgorithm import BinaryGeneticAlgorithm
 
+def parse_args():
+    import argparse
+    import re
+    acceptable_solution_length = 64
+    
+    parser = argparse.ArgumentParser(description='Simple Binary Genetic Algorithm.')
+    parser.add_argument('-s', '--solution',
+                        help='path to torrent file',
+                        dest='solution',
+                        required=True,
+                        type=str)
+    
+    args = parser.parse_args()
+    solution = args.solution
+    
+    if len(solution) != acceptable_solution_length:
+        raise Exception('solution has invalid length of %d should be %d' % (
+        len(solution), acceptable_solution_length))
+    
+    pattern = r'[^\.0-1]'
+    if re.search(pattern, solution):
+        raise Exception('solution contains invalid character %d' % (len(solution),))
+        
+    return solution
+    
+def main():
+    solution = parse_args()
+    binary_genetic_algo = BinaryGeneticAlgorithm(solution)
+    
 if __name__ == '__main__':
-    binary_genetic_algo = BinaryGeneticAlgorithm(
-        '1111000000000000000000000000000000000000000000000000000000001111')
+    main()
